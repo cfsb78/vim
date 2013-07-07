6b5d2e0458eb18faab0d7dd0c2704aed06deed10
Ubuntu <ubuntu@ip-10-251-56-200.us-west-2.compute.internal>
2013-07-07 01:12:00 +0000

    Installed  as submodule

--- add-module.sh
+++ add-module.sh
@@ -14,9 +14,16 @@ show_usage()
 }
 add_module()
 {
+  echo "Adding submodule $1 to bundle/$1 ..."
   git submodule add $2 bundle/$1
+  echo "Initializing $1 submodule..."
+  git submodule init bundle/$1
+  echo "Updating $1 submodule..."
+  git submodule update bundle/$1
+  echo "Adding new files and commiting..."
   git add .
   git commit -m "Installed $1 as submodule"
+  echo "Changes committed! Run 'git push origin' to complete delivery."
 }
 
 if [ $# -lt 2 ];
